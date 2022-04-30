4787b4e9ee72e3ea0b9fd61bedaea6e67b54f705
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-30 14:25:28 +0530

    Created babel file to fix a error.

---
+++ client/pages/api/auth/[...nextauth].js
@@ -0,0 +1,21 @@
+// Authentication
+
+import NextAuth from 'next-auth';
+import EmailProvider from "next-auth/providers/email";
+
+
+export default NextAuth({
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
